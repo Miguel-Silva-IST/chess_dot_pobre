84423001b37a0a42a2350d2e34e1b3d4cd04704f
mntsilva <mntsilva@extworten.pt>
2024-05-26 18:26:03 +0100

    Added mapping to board

--- app/board.py
+++ app/board.py
@@ -1,8 +1,34 @@
-    #1-initializes the board positions
-    #2-each position represented by either - P-pawn, K - knight , B -bishop, R - rook, Q-queen, KI-king 
-    #3 - does it make scense to do one class per piece???
-    #choose identifier of white and black
+from enum import Enum
 
+class BoardMapping(Enum):
+    """
+    ex. WK - white knight
+    
+    1st char:
+    W - white
+    B - black
+    
+    2nd char:
+    P - pawn
+    R - rook
+    K - knight
+    B - bishop
+    Q - queen
+    KI - king
+    """
+    
+    WP = -1
+    WR = -2
+    WK = -3
+    WB = -4
+    WQ = -5
+    WKI = -6
+    BP = 1
+    BR = 2
+    BK = 3
+    BB = 4
+    BQ = 5
+    BKI = 6
 
 
 
@@ -11,20 +37,21 @@ class Board:
     def __init__(self):
         self.board = [[None for c in range(8)] for r in range(8)]
         for c in range(8):
-            self.board[1][c] = 'WP'
-            self.board[6][c] = 'BP'
+            self.board[1][c] = -1
+            self.board[6][c] = 1
         
-        self.board[0][0],self.board[0][7],self.board[7][0],self.board[7][7] = 'WR','WR','BR','BR'
-        self.board[0][1],self.board[0][6],self.board[7][1],self.board[7][6] = 'WK','WK','BK','BK'
-        self.board[0][2],self.board[0][5],self.board[7][2],self.board[7][5] = 'WB','WB','BB','BB'
-        self.board[0][3], self.board[7][3] = 'WQ','BQ'
-        self.board[0][4], self.board[7][4] = 'WKI','BKI'
+        self.board[0][0],self.board[0][7],self.board[7][0],self.board[7][7] = -2,-2,2,2
+        self.board[0][1],self.board[0][6],self.board[7][1],self.board[7][6] = -3,-3,3,3
+        self.board[0][2],self.board[0][5],self.board[7][2],self.board[7][5] = -4,-4,4,4
+        self.board[0][3], self.board[7][3] = -5,5
+        self.board[0][4], self.board[7][4] = -6,6
 
     
     def __repr__(self):
-        board_str = ''
+        board_str = '' 
         for row in self.board:
-            board_str+=f'{row}\n'
+            aux_board_row = map(lambda x: BoardMapping(x).name, row)
+            board_str+=f'{aux_board_row}\n'
         
         return board_str
             
